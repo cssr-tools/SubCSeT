feb18e2260f3c7bb07df6b02b4ea4e52d5151389
Alexey Khrulenko <alkh@norceresearch.no>
2026-03-07 08:06:30 +0100

    scraps periods dark theme

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ themes_options=[{'label': i, 'value': eval('dbc.themes.'+i.upper())} \
 # theme0 = "bootstrap"  # sets the theme
 # theme0 = "journal"  # sets the theme
 # theme0 = "sandstone"
-light_theme = (datetime.today() - datetime(2026,3,3)).days % 3 != 0
+# light_theme = (datetime.today() - datetime(2026,3,3)).days % 3 != 0
+light_theme = True
 theme0 = "cerulean" if light_theme else "cyborg"  # sets the theme
 map_style0 = "basic" if light_theme else "carto-darkmatter"
 
